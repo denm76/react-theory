92e7710c7243f00ba6c4e901de73ff177cfaec23
denm76 <denm76@gmail.com>
2021-09-17 13:20:41 +0300

    Simple component add by different variants

---
+++ src/Car/Car.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// function car(){
+//     return(
+//         <h2>This is car component</h2>
+//     )
+// }
+// const car = () => {
+//     return(<h1>This is car component</h1>)
+// }
+
+// const car = () => <h1>
+//     This is car component
+//     <br/>
+//     <strong>Test</strong>
+// </h1>
+
+export default () =><h1>This is car component<br/><strong>Test</strong></h1>

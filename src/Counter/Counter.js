588ff169bf9d6135b67c8297db93ac1670c80211
denm76 <denm76@gmail.com>
2021-10-08 15:49:36 +0300

    Использованы способы замены корневого контейнера компонента:Reac.Fragment, Auxiliary, <> </>

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Auxiliary from "../hoc/Auxiliary";
 
 export default class Counter extends Component{
     state = {
@@ -13,18 +14,23 @@ export default class Counter extends Component{
     }
     render() {
 
-        // return (
-        //     <div>
-        //         <h2>Counter{this.state.counter}</h2>
-        //         <button onClick={this.addCounter}>+</button>
-        //         <button onClick={() => this.setState({counter:this.state.counter-1})}>-</button>
-        //     </div>
-        // )
+        return (
+            // <React.Fragment>
+            //<Auxiliary>
+            <>
+                <h2>Counter{this.state.counter}</h2>
+                <button onClick={this.addCounter}>+</button>
+                <button onClick={() => this.setState({counter:this.state.counter-1})}>-</button>
+            </>
+            //</Auxiliary>
 
-        return [
-                <h2 key={'1'}>Counter{this.state.counter}</h2>,
-                <button key={'2'} onClick={this.addCounter}>+</button>,
-                <button key={'3'} onClick={() => this.setState({counter:this.state.counter-1})}>-</button>
-        ]
+            //</React.Fragment>
+        )
+
+        // return [
+        //         <h2 key={'1'}>Counter{this.state.counter}</h2>,
+        //         <button key={'2'} onClick={this.addCounter}>+</button>,
+        //         <button key={'3'} onClick={() => this.setState({counter:this.state.counter-1})}>-</button>
+        // ]
     }
 }
